e11557fc62fc4d3214ee4c79eec0da05f1aede17
Harpreet Sangha <hasangha04@gmail.com>
2023-03-25 18:03:29 -0700

    can keep pressing button to play again

--- button.js
+++ button.js
@@ -29,6 +29,11 @@ function addSoundsToPage(sounds)
         player.setAttribute('src', `https://hasangha04.github.io/Soundboards/${sound.src}`);
 
         playButton.addEventListener('click', () => {
+            if (player.currentTime >= player.duration)
+            {
+                player.currentTime = 0;
+            }
+            
             player.play();
         });
 
