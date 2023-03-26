9d5a247ead425657e8e08f7563544f59e0d8ba14
Harpreet Sangha <hasangha04@gmail.com>
2023-03-25 17:48:32 -0700

    Adding new files

---
+++ button.js
@@ -0,0 +1,37 @@
+const soundsElement = document.querySelector('#sounds');
+
+(async () => {
+    const sounds = await getSounds();
+    addSoundsToPage(sounds);
+})();
+
+async function getSounds() {
+  const response = await fetch('https://hasangha04.github.io/SoundBoard/sounds.json');
+  const json = await response.json();
+  return json;
+}
+
+function addSoundsToPage(sounds)
+{
+    sounds.forEach(sound => {
+        const soundDiv = document.createElement('div');
+        soundDiv.className = 'sound';
+        const soundTitle = document.createElement('h2');
+        soundTitle.textContent = sound.title;
+        soundDiv.appendChild(soundTitle);
+
+        const playButton = document.createElement('button');
+        playButton.textContent = '▶️';
+        soundDiv.appendChild(playButton);
+
+        const player = document.createElement('audio');
+        soundDiv.appendChild(player);
+        player.setAttribute('src', `https://hasangha04.github.io/SoundBoard/sounds/${sound.src}`);
+
+        playButton.addEventListener('click', () => {
+            player.play();
+        });
+
+        soundsElement.appendChild(soundDiv);
+    });
+}
