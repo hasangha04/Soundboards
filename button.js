912be2b59c90582e600f608020a3d9f3c8c7ddb4
Harpreet Sangha <hasangha04@gmail.com>
2023-03-25 21:54:43 -0700

    changed set attribute

--- button.js
+++ button.js
@@ -1,16 +1,3 @@
-const soundsElement = document.querySelector('#sounds');
-
-(async () => {
-    const sounds = await getSounds();
-    addSoundsToPage(sounds);
-})();
-
-async function getSounds() {
-  const response = await fetch('https://hasangha04.github.io/Soundboards/sounds.json');
-  const json = await response.json();
-  return json;
-}
-
 function addSoundsToPage(sounds) {
     const soundsElement = document.getElementById('sounds');
   
@@ -18,31 +5,13 @@ function addSoundsToPage(sounds) {
       const soundDiv = document.createElement('div');
       soundDiv.className = 'sound';
   
-      // Create an input element for selecting image file
-      const imageInput = document.createElement('input');
-      imageInput.type = 'file';
-      imageInput.accept = 'image/*';
-      soundDiv.appendChild(imageInput);
-  
       const player = document.createElement('audio');
       soundDiv.appendChild(player);
-      player.setAttribute('src', `https://hasangha04.github.io/Soundboards/${sound.src}`);
+      player.setAttribute('src', `https://hasangha04.github.io/Soundboards/${sound.audioSrc}`);
   
-      // Create an image element for displaying selected image
       const image = document.createElement('img');
       soundDiv.appendChild(image);
-  
-      // Listen for file input change event
-      imageInput.addEventListener('change', () => {
-        const file = imageInput.files[0];
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => {
-          image.src = reader.result;
-          // Hide the input element after an image is selected
-          imageInput.style.display = 'none';
-        };
-      });
+      image.setAttribute('src', `https://hasangha04.github.io/Soundboards/${sound.imageSrc}`);
   
       // Listen for image click event to play sound
       image.addEventListener('click', () => {
@@ -69,30 +38,11 @@ function addSoundsToPage(sounds) {
       const newSoundDiv = document.createElement('div');
       newSoundDiv.className = 'sound';
   
-      // Create an input element for selecting image file
-      const newImageInput = document.createElement('input');
-      newImageInput.type = 'file';
-      newImageInput.accept = 'image/*';
-      newSoundDiv.appendChild(newImageInput);
-  
       const newPlayer = document.createElement('audio');
       newSoundDiv.appendChild(newPlayer);
   
-      // Create an image element for displaying selected image
       const newImage = document.createElement('img');
       newSoundDiv.appendChild(newImage);
-  
-      // Listen for file input change event
-      newImageInput.addEventListener('change', () => {
-        const file = newImageInput.files[0];
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => {
-          newImage.src = reader.result;
-          // Hide the input element after an image is selected
-          newImageInput.style.display = 'none';
-        };
-      });
   
       // Listen for image click event to play sound
       newImage.addEventListener('click', () => {
@@ -103,7 +53,4 @@ function addSoundsToPage(sounds) {
       soundsElement.appendChild(newSoundDiv);
     });
   }
-  
-  // Call the function to add sounds to page
-  addSoundsToPage(sounds);
   
